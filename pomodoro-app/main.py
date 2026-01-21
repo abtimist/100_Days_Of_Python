d9a7023e907eea694e5d2bdb64ad0a82675030ca
Abtimist <vasagam.abhishek@gmail.com>
2026-01-21 22:22:14 +0530

    Initial Functions

---
+++ pomodoro-app/main.py
@@ -0,0 +1,54 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start():
+    count_down(5*60)
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    canvas.itemconfig(time_text, text= f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=100, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224,bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100,112, image=tomato_img)
+time_text = canvas.create_text(100,130,text="00:00",fill="white",font=(FONT_NAME, 35,"bold"))
+canvas.grid(column=1,row=1)
+
+timer_text = Label(text="Timer",font=(FONT_NAME,48,"normal"),fg=GREEN,bg=YELLOW)
+timer_text.grid(column=1,row=0)
+
+
+def reset():
+    pass
+
+start_button = Button(text="START",highlightthickness=0,bg="green",fg="white",command=start)
+start_button.grid(column=0,row=2)
+
+reset_button = Button(text="RESET",highlightthickness=0,bg=RED,fg="white", command=reset)
+reset_button.grid(column=2,row=2)
+
+checkmark = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME,20,"normal"))
+checkmark.grid(column=1,row=3)
+
+window.mainloop()
