b446b2b87729216dc1e8774bbffaa970c970311d
Abtimist <vasagam.abhishek@gmail.com>
2026-03-26 22:39:49 +0530

    Project using API

---
+++ habit-tracker/main.py
@@ -0,0 +1,59 @@
+import os
+
+import requests
+from datetime import datetime
+USERNAME="YOUR_USERNAME"
+TOKEN="YOUR_TOKEN"
+GRAPHID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token":TOKEN,
+    "username":USERNAME,
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
+}
+# response = requests.post(url=pixela_endpoint,json=user_params)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":GRAPHID,
+    "name":"Cycling Graph",
+    "unit":"Km",
+    "type":"float",
+    "color":"ajisai"
+}
+headers = {
+    "X-USER-TOKEN":TOKEN
+}
+# response = requests.post(url=graph_endpoint,json=graph_config,headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity":"25",
+}
+
+
+response = requests.post(url=pixel_creation_endpoint,json=pixel_data,headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime("%Y%m%d")}"
+
+new_pixel_data = {
+    "quantity": "4.5"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPHID}/{today.strftime("%Y%m%d")}"
+
+# response = requests.delete(url=delete_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
