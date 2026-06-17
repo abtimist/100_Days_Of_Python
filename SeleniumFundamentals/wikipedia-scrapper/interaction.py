1ce0666085aa998f9f6ddf757b7def689d64d4e2
Abtimist <vasagam.abhishek@gmail.com>
2026-06-17 23:43:24 +0530

    revert: restore original Day 48 files as written

--- SeleniumFundamentals/wikipedia-scrapper/interaction.py
+++ SeleniumFundamentals/wikipedia-scrapper/interaction.py
@@ -3,15 +3,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option('detach', True)
+chrome_options.add_experimental_option('detach',True)
 chrome_options.add_argument("--force-device-scale-factor=0.20")
 
 driver = webdriver.Chrome(options=chrome_options)
+# driver.get('https://en.wikipedia.org/wiki/Main_Page')
 driver.get('https://appbrewery.github.io/fake-newsletter-signup/')
+# article_count = driver.find_element(By.CSS_SELECTOR,value='#articlecount ul li:nth-child(2) a')
+# article_count.click()
+# print(article_count.text)
 
-name_tags = ['fName', 'lName', 'email']
-details = ['Absolute', 'World', 'Absworld@gmail.com']
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+name_tags =['fName','lName','email']
+details = ['Absolute','World','Absworld@gmail.com']
 for i in range(len(name_tags)):
     search = driver.find_element(By.NAME, value=name_tags[i])
     search.send_keys(details[i])
     search.send_keys(Keys.ENTER)
+
+
+
+
+# print(driver.get_window_size())
+# print(search.get_attribute("outerHTML"))
+
+
+
--- automated-cookie-clicker/main.py
+++ automated-cookie-clicker/main.py
@@ -1,39 +1,67 @@
+
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 chrome_options.add_argument('--force-device-scale-factor=0.20')
 
+
 driver = webdriver.Chrome(options=chrome_options)
+
 driver.get('https://ozh.github.io/cookieclicker/')
+
+# time.sleep(1)
 
 wait = WebDriverWait(driver, 10)
 
 language_element = wait.until(
     EC.element_to_be_clickable((By.ID, "langSelect-EN"))
 )
+
 language_element.click()
 
 time.sleep(2)
 
-cookie_element = driver.find_element(By.ID, value='bigCookie')
+cookie_element = driver.find_element(By.ID,value='bigCookie')
 
 start_time = time.time() + 300
 
-while time.time() <= start_time:
+while time.time()<=start_time:
     timeout = time.time() + 2
-    while time.time() < timeout:
+    while time.time()<timeout:
         cookie_element.click()
-    products = driver.find_elements(By.CSS_SELECTOR, value="#products .product.unlocked.enabled")
+    products = driver.find_elements(By.CSS_SELECTOR,value="#products .product.unlocked.enabled")
     upgrades = driver.find_elements(By.CSS_SELECTOR, value="#upgrades .crate.upgrade.enabled")
     if len(upgrades) != 0:
         upgrades[-1].click()
-    if len(products) != 0:
+    if len(products)!=0:
         products[-1].click()
 
-print(f"Cookies Per Second = {driver.find_element(By.ID, value='cookiesPerSecond').text}")
+
+'''TEST'''
+# count = 0
+# while True:
+#     cookie_element.click()
+#     products = driver.find_elements(By.CSS_SELECTOR,value="#products .product.unlocked.enabled")
+#     upgrades = driver.find_elements(By.CSS_SELECTOR,value="#upgrades .crate.upgrade.enabled")
+#     if len(upgrades)!=0:
+#         upgrades[-1].click()
+#     if len(products)!=0:
+#         products[-1].click()
+#     count+=1
+#     if count>200:
+#         break
+#
+# upgrades = driver.find_elements(By.CSS_SELECTOR,value="#upgrades .crate.upgrade")
+# print([i.text for i in upgrades])
+
+
+print(f"Cookies Per Second = {driver.find_element(By.ID,value="cookiesPerSecond").text}")
 driver.quit()
+
+
